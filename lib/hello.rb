58bcf3337e03c6b5a24e4f81932aa04bbc17a4c4
Britny Edwards <bredwards2@gmail.com>
2018-11-25 14:27:22 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,13 +2,12 @@ def hello_t
    i = 0
  
   while i < array.length
-    yield array[i]
+    yield (array[i])
     i = i + 1
   end
   
-  
   array
-  else
+else
     puts "Hey! No block was given!"
   end
 end
